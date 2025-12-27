a8a7067b2bfd75b322691c9658930b4366ed652b
tsukimizake <shomasd@gmail.com>
2025-12-27 21:11:27 +0900

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,158 +1,172 @@
 {
-    description = "A flake to provision my environment";
+  description = "A flake to provision my environment";
 
-    inputs = {
-        nixpkgs = {
-            url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        };
-
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        nix-darwin = {
-            url = "github:LnL7/nix-darwin";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
 
-    outputs = {
-        self,
-        nixpkgs,
-        home-manager,
-        nix-darwin,
-    }: {
-        darwinConfigurations = {
-            "tsukimizakenoMacBook-Pro" = nix-darwin.lib.darwinSystem {
-                system = "aarch64-darwin";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-                modules = [
-                    ({ config, pkgs, ... }: {
-                        system = {
-                            stateVersion = 5;
-                            primaryUser = "t";
-                        };
-                        users.users.t = {
-                            name = "t";
-                            home = "/Users/t";
-                        };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-			environment.systemPath = [
-			  "/opt/homebrew/bin"
-			  "/opt/homebrew/sbin"
-			];
-			environment.systemPackages = [
-			  (pkgs.writeTextFile {
-			    name = "nu_in_nvim";
-			    destination = "/bin/nu_in_nvim";
-			    executable = true;
-			    text = ''
-#!/usr/bin/env nu
-def main [--cmd : string ] {
-  if $cmd != null {
-    nu -c $cmd
-  } else {
-    nu -e "$env.RUN_IN_NVIM = true"
-  }
-}
-			    '';
-			  })
-			  pkgs.nodejs
-			];
-                        homebrew = {
-                            enable = true;
-			    user = "t";
-			    taps = [
-				"daipeihust/tap"
-			    ];
-                            brews = [
-                              "autoconf"
-                              "automake"
-                              "awscli"
-                              "cmigemo"
-                              "coreutils"
-                              "direnv"
-                              "emscripten"
-                              "ffmpeg"
-                              "fzf"
-                              "gh"
-                              "ghcup"
-                              "git"
-                              "gnuplot"
-                              "go"
-                              "guile"
-                              "harfbuzz"
-                              "im-select"
-                              "imagemagick"
-                              "jq"
-                              "just"
-                              "libtool"
-                              "libyaml"
-                              "luarocks"
-                              "mise"
-                              { name = "neovim"; args = [ "HEAD" ]; }
-                              "neovim-remote"
-                              "nim"
-                              "ninja"
-                              "nushell"
-                              "opam"
-                              "openssl@3"
-                              "pkgconf"
-                              "poppler"
-                              "protobuf"
-                              "python-matplotlib"
-                              # "qmk"
-                              "readline"
-                              "redo"
-                              "ripgrep"
-                              "rlwrap"
-                              "sevenzip"
-                              "shellcheck"
-                              "sox"
-                              "swi-prolog"
-                              "terminal-notifier"
-                              "tesseract"
-                              "tmux"
-                              "tree"
-                              "tree-sitter"
-                              "unixodbc"
-                              "unzip"
-                              "uv"
-                              "vhs"
-                              "wasm-tools"
-                              "wasmtime"
-                              "wxwidgets"
-                              ];
-                            casks = [
-                              "alacritty"
-                              "amethyst"
-                              "codex"
-                              "copilot-cli"
-                              "discord"
-                              "docker"
-                              "font-hackgen-nerd"
-                              "openscad@snapshot"
-                              "slack"
-                              "steam"
-                              "vnc-viewer"
-                            ];
-                        };
-                    })
-                    home-manager.darwinModules.home-manager
-                    ({ ... }: {
-                        home-manager = {
-                            users.t = { ... }: {
-                                home.stateVersion = "23.11";
-                                programs.tmux = {
-                                    enable = true;
-                                    extraConfig = builtins.readFile ./tmux.conf;
-                                };
-                            };
-                        };
-                    })
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+    }:
+    {
+      darwinConfigurations = {
+        "tsukimizakenoMacBook-Pro" = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+
+          modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                system = {
+                  stateVersion = 5;
+                  primaryUser = "t";
+                };
+                users.users.t = {
+                  name = "t";
+                  home = "/Users/t";
+                };
+
+                environment.systemPath = [
+                  "/opt/homebrew/bin"
+                  "/opt/homebrew/sbin"
                 ];
-            };
+                environment.systemPackages = [
+                  (pkgs.writeTextFile {
+                    name = "nu_in_nvim";
+                    destination = "/bin/nu_in_nvim";
+                    executable = true;
+                    text = ''
+                      #!/usr/bin/env nu
+                      def main [--cmd : string ] {
+                        if $cmd != null {
+                          nu -c $cmd
+                        } else {
+                          nu -e "$env.RUN_IN_NVIM = true"
+                        }
+                      }
+                      			    '';
+                  })
+                  pkgs.nixfmt
+                  pkgs.nodejs
+                ];
+                homebrew = {
+                  enable = true;
+                  user = "t";
+                  taps = [
+                    "daipeihust/tap"
+                  ];
+                  brews = [
+                    "autoconf"
+                    "automake"
+                    "awscli"
+                    "cmigemo"
+                    "coreutils"
+                    "direnv"
+                    "emscripten"
+                    "ffmpeg"
+                    "fzf"
+                    "gh"
+                    "ghcup"
+                    "git"
+                    "gnuplot"
+                    "go"
+                    "guile"
+                    "harfbuzz"
+                    "im-select"
+                    "imagemagick"
+                    "jq"
+                    "just"
+                    "libtool"
+                    "libyaml"
+                    "luarocks"
+                    "mise"
+                    {
+                      name = "neovim";
+                      args = [ "HEAD" ];
+                    }
+                    "neovim-remote"
+                    "nim"
+                    "ninja"
+                    "nushell"
+                    "opam"
+                    "openssl@3"
+                    "pkgconf"
+                    "poppler"
+                    "protobuf"
+                    "python-matplotlib"
+                    # "qmk"
+                    "readline"
+                    "redo"
+                    "ripgrep"
+                    "rlwrap"
+                    "sevenzip"
+                    "shellcheck"
+                    "sox"
+                    "swi-prolog"
+                    "terminal-notifier"
+                    "tesseract"
+                    "tmux"
+                    "tree"
+                    "tree-sitter"
+                    "unixodbc"
+                    "unzip"
+                    "uv"
+                    "vhs"
+                    "wasm-tools"
+                    "wasmtime"
+                    "wxwidgets"
+                  ];
+                  casks = [
+                    "alacritty"
+                    "amethyst"
+                    "codex"
+                    "copilot-cli"
+                    "discord"
+                    "docker"
+                    "font-hackgen-nerd"
+                    "openscad@snapshot"
+                    "slack"
+                    "steam"
+                    "vnc-viewer"
+                  ];
+                };
+              }
+            )
+            home-manager.darwinModules.home-manager
+            (
+              { ... }:
+              {
+                home-manager = {
+                  users.t =
+                    { ... }:
+                    {
+                      home.stateVersion = "23.11";
+                      programs.tmux = {
+                        enable = true;
+                        extraConfig = builtins.readFile ./tmux.conf;
+                      };
+                    };
+                };
+              }
+            )
+          ];
         };
+      };
     };
 }
